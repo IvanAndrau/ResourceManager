5b3a54fe2f72be39aca5bfb204f940aef0503820
Ivan <ivan.andrau@gmail.com>
2024-12-14 16:12:02 +0100

    first commit

---
+++ src/main/java/org/example/resourcemanagerwebtask/ResourceManagerWebTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.resourcemanagerwebtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagerWebTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceManagerWebTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/resourcemanagerwebtask/ResourceManagerWebTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resourcemanagerwebtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagerWebTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
